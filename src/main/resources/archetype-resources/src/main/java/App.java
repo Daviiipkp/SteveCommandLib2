ae9c50d0c855a6beec9d8445652c9e5171de6f23
Daviiipkp <85971755+Daviiipkp@users.noreply.github.com>
2026-01-29 18:42:53 -0300

    initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.daviipkp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
